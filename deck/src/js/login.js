e5359849233124fd6700383f5f287f54d87e7e04
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2023-05-20 17:19:45 +0800

    🔧chore: Add eslint

--- deck/src/js/login.js
+++ deck/src/js/login.js
@@ -1,37 +1,37 @@
 /** @format */
 
 // src/index.js
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
   const submitForm = () => {
     $.ajax({
-      type: "POST",
-      url: "/r'%5Elogin/$'",
+      type: 'POST',
+      url: '/r\'%5Elogin/$\'',
       data: {
         username: username,
         password: password,
-        to_redirect: to_redirect,
+        to_redirect: toRedirect,
         csrfmiddlewaretoken: CSRF_TOKEN,
       },
-      success: function (data) {
-        window.location.href = (data.to_redirect == "None" ? "/" : data.to_redirect);
+      success: function(data) {
+        window.location.href =
+          data.toRedirect == 'None' ? '/' : data.toRedirect;
       },
-      error: function (xhr, status, error) {
-        console.log("Login failed:", error);
-        setMessage(JSON.parse(xhr["responseText"]).responseText);
+      error: function(xhr, status, error) {
+        console.log('Login failed:', error);
+        setMessage(JSON.parse(xhr['responseText']).responseText);
       },
     });
   };
-
   return (
     <div className="login-div">
-      <img src="/static/inventory/img/St John Singapore Logo (Inverted, Transparent Background).png"></img>
+      <img src="/static/inventory/img/StJohn SG logo.png"></img>
       <p className="lead error-message">{message}</p>
       <input
         className="form-control"
@@ -51,5 +51,5 @@ const App = () => {
   );
 };
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<App />);
