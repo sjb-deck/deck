d473ec9369864c833792e952f31727e6b05a6782
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2023-05-25 18:29:24 +0800

    🐛 fix: Fix undefined routing after login

--- deck/src/js/login.js
+++ deck/src/js/login.js
@@ -1,55 +1,55 @@
 /** @format */
 
 // src/index.js
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
 const App = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [message, setMessage] = useState("");
 
-  const submitForm = () => {
-    $.ajax({
-      type: 'POST',
-      url: '/r\'%5Elogin/$\'',
-      data: {
-        username: username,
-        password: password,
-        to_redirect: toRedirect,
-        csrfmiddlewaretoken: CSRF_TOKEN,
-      },
-      success: function(data) {
-        window.location.href =
-          data.toRedirect == 'None' ? '/' : data.toRedirect;
-      },
-      error: function(xhr, status, error) {
-        console.log('Login failed:', error);
-        setMessage(JSON.parse(xhr['responseText']).responseText);
-      },
-    });
-  };
-  return (
-    <div className="login-div">
-      <img src="/static/inventory/img/StJohn SG logo.png"></img>
-      <p className="lead error-message">{message}</p>
-      <input
-        className="form-control"
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
-      ></input>
-      <input
-        className="form-control"
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      ></input>
-      <button className="btn btn-primary" type="submit" onClick={submitForm}>
-        Login
-      </button>
-    </div>
-  );
+	const submitForm = () => {
+		$.ajax({
+			type: "POST",
+			url: "/r'%5Elogin/$'",
+			data: {
+				username: username,
+				password: password,
+				to_redirect: toRedirect,
+				csrfmiddlewaretoken: CSRF_TOKEN,
+			},
+			success: function (data) {
+				window.location.href =
+					data.toRedirect == undefined ? "/" : data.toRedirect;
+			},
+			error: function (xhr, status, error) {
+				console.log("Login failed:", error);
+				setMessage(JSON.parse(xhr["responseText"]).responseText);
+			},
+		});
+	};
+	return (
+		<div className='login-div'>
+			<img src='/static/inventory/img/StJohn SG logo.png'></img>
+			<p className='lead error-message'>{message}</p>
+			<input
+				className='form-control'
+				placeholder='Username'
+				onChange={(e) => setUsername(e.target.value)}
+			></input>
+			<input
+				className='form-control'
+				type='password'
+				placeholder='Password'
+				onChange={(e) => setPassword(e.target.value)}
+			></input>
+			<button className='btn btn-primary' type='submit' onClick={submitForm}>
+				Login
+			</button>
+		</div>
+	);
 };
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(<App />);
