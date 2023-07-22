5d12b2231cc9a797cf3009af2d35ba6899aa1827
Wee Ming Qing <105474691+WeeMingQing@users.noreply.github.com>
2023-07-22 13:24:13 +0800

    Add new expiry (#67)
    Co-authored-by: jonasgwt <55698556+jonasgwt@users.noreply.github.com>

--- inventory/views.py
+++ inventory/views.py
@@ -180,6 +180,26 @@ def add_expiry_post(request):
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
+def create_new_expiry(request):
+    if request.method == "POST":
+        serializer = ItemExpiryDateSerializer(data=request.data)
+        if serializer.is_valid():
+            quantityopen = serializer.validated_data.get("quantityopen")
+            quantityunopened = serializer.validated_data.get("quantityunopened")
+            serializer.validated_data["quantityopen"] = 0
+            serializer.validated_data["quantityunopened"] = 0
+            serializer.save()
+            obj_id = serializer.instance.pk
+            obj = ItemExpiry.objects.filter(id=obj_id)[0]
+            obj.deposit(quantityopen, quantityunopened)
+
+            return Response({"Message": "Created expiryItem successfully"}, status=200)
+        else:
+            return Response({"Error": "Bad Request"}, status=400)
+    else:
+        return Response({"Error": "Invalid request method"}, status=405)
+
+
 def loan_return_post(request):
     if request.method == "POST":
         loan_return_serializer = LoanReturnSerializer(data=request.data)
