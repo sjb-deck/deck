60c55021b99de83244efeb682938ce74cec163cb
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2023-07-22 13:19:16 +0800

    Fix version incompatibility

--- __tests__/SearchBar.test.js
+++ __tests__/SearchBar.test.js
@@ -1,7 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 
-import '@testing-library/jest-dom';
 import SearchBar from '../components/SearchBar';
 
 const mockItems = [
