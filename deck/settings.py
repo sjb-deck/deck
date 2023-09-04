e63d09826a5400a243918e3781c7295dd04af715
jonasgwt <jongoh2000@gmail.com>
2023-09-04 19:48:54 +0800

    Fix CI

--- deck/settings.py
+++ deck/settings.py
@@ -90,7 +90,7 @@ if IN_GITHUB_ACTIONS:
             "ENGINE": "django.db.backends.mysql",
             "NAME": "test_deck_staging",
             "USER": "root",
-            "PASSWORD": config("TEST_DB_PW"),
+            "PASSWORD": "testrootpassword",
             "HOST": "127.0.0.1",
             "PORT": "3306",
         }
