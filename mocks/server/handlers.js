72adc1e586f52094d2939ee8fcf64ea5dce9216a
sweic <cswei019@gmail.com>
2023-09-10 11:16:01 +0800

    Created tests for AdminIndex

--- mocks/server/handlers.js
+++ mocks/server/handlers.js
@@ -3,11 +3,11 @@ import { rest } from 'msw';
 
 import {
   INV_API_ITEMS_URL,
+  INV_API_ORDERS_URL,
   INV_API_SUBMIT_ORDER_URL,
   INV_API_USER_URL,
 } from '../../globals';
-import { mockItems } from '../items';
-import { mockUser } from '../user';
+import { mockItems, mockOrders, mockUser } from '../index';
 
 export const handlers = [
   rest.get(INV_API_USER_URL, (req, res, ctx) => {
@@ -16,6 +16,14 @@ export const handlers = [
 
   rest.get(INV_API_ITEMS_URL, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockItems));
+  }),
+
+  rest.get(INV_API_ORDERS_URL, (req, res, ctx) => {
+    const orders = new Array(5)
+      .fill(mockOrders)
+      .flat()
+      .map((o, idx) => ({ ...o, id: idx + 100 }));
+    return res(ctx.status(200), ctx.json(orders));
   }),
 
   rest.post(INV_API_SUBMIT_ORDER_URL, (req, res, ctx) => {
