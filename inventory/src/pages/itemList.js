3d69c5159cec36a5cb7f5bf0f3d18797f7a65c63
jonasgwt <jongoh2000@gmail.com>
2023-10-27 13:39:11 +0800

    Add loading spinners in itemList

--- inventory/src/pages/itemList.js
+++ inventory/src/pages/itemList.js
@@ -5,7 +5,6 @@ import {
   Footer,
   LoadingSpinner,
   NavBar,
-  SnackBarAlerts,
   ImportModal,
   ItemTable,
 } from '../components';
@@ -15,22 +14,18 @@ import { useItems, useUser } from '../hooks/queries';
 import '../globals/styles/inventoryBase.scss';
 
 export const ItemList = () => {
-  const { data: user, loading: userLoading, error: userError } = useUser();
-  const { data: items, loading: itemsLoading, error: itemsError } = useItems();
-  const { mutate: onExportClick } = useExportItems();
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const { data: user, isLoading: userLoading, error: userError } = useUser();
+  const { data: items, isLoading: itemsLoading } = useItems();
+  const { mutate: onExportClick, isLoading: exportLoading } = useExportItems();
   const [userData, setUserData] = useState(user);
   const [modalOpen, setModalOpen] = useState(false);
   const [expiryDates, setExpiryDates] = useState([]);
 
   useEffect(() => {
-    if (userError || itemsError) {
-      setSnackbarOpen(true);
-    }
     if (!userLoading && !userError) {
       setUserData(user);
     }
-  }, [userLoading, userError, user, itemsError]);
+  }, [userLoading, userError, user]);
 
   useEffect(() => {
     if (!items) return;
@@ -49,16 +44,9 @@ export const ItemList = () => {
   return (
     <>
       <NavBar user={userData} />
-      <SnackBarAlerts open={snackbarOpen} message={userError?.message} />
+      {(itemsLoading || exportLoading) && <LoadingSpinner />}
       <Box
-        sx={{
-          display: 'flex',
-
-          justifyContent: 'center',
-          marginTop: 10,
-        }}
-      ></Box>
-      <Box
+        className='nav-margin-compensate'
         sx={{
           width: 1,
           minHeight: 0.8,
@@ -70,22 +58,28 @@ export const ItemList = () => {
         <ImportModal open={modalOpen} setOpen={setModalOpen} />
         <ButtonGroup
           aria-label='text button group'
+          className='dynamic-width'
           sx={{
             display: 'flex',
             flexWrap: 'wrap',
             justifyContent: 'center',
             marginBottom: 1,
-            width: { xs: '90%', sm: '70%', md: '70%', lg: '45%', xl: '35%' },
           }}
         >
-          <Button onClick={() => setModalOpen(true)}>Import</Button>
-          <Button onClick={onExportClick}>Export</Button>
+          <Button
+            onClick={() => setModalOpen(true)}
+            disabled={itemsLoading || exportLoading}
+          >
+            Import
+          </Button>
+          <Button
+            onClick={onExportClick}
+            disabled={itemsLoading || exportLoading}
+          >
+            Export
+          </Button>
         </ButtonGroup>
-        {itemsLoading ? (
-          <LoadingSpinner />
-        ) : (
-          expiryDates && <ItemTable items={expiryDates} />
-        )}
+        {!!expiryDates.length && <ItemTable items={expiryDates} />}
       </Box>
       <Footer />
     </>
