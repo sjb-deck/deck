0ce741d2749c3a2f162e8217923cce44e707ae5a
cheehengk <cheehengk@yahoo.com.sg>
2023-10-26 21:12:03 -0400

    :tada: Add basic kits endpoints

--- inventory/kits/serializers.py
+++ inventory/kits/serializers.py
@@ -3,3 +3,39 @@ from rest_framework import serializers
 from rest_framework.relations import PrimaryKeyRelatedField
 
 from accounts.models import User, UserExtras
+
+from .models import *
+
+
+class KitSerializer(serializers.ModelSerializer):
+    complete = serializers.SerializerMethodField()
+    blueprint_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Kit
+        fields = ['id', 'name', 'status', 'content', 'blueprint_name', 'complete']
+
+    @staticmethod
+    def get_complete(obj):
+        kit_content = obj.content
+        blueprint_content = obj.blueprint.complete_content
+
+        # Check if they have the same set of keys
+        if set(kit_content.keys()) != set(blueprint_content.keys()):
+            return "item-mismatch"
+
+        # Compare individual fields
+        for key, kit_value in kit_content.items():
+            blueprint_value = blueprint_content.get(key)
+
+            if kit_value > blueprint_value:
+                return "overloaded"
+            elif kit_value < blueprint_value:
+                return "incomplete"
+
+        # If the loop completes without returning, all fields are equal
+        return "complete"
+
+    @staticmethod
+    def get_blueprint_name(obj):
+        return obj.blueprint.name
--- inventory/kits/views.py
+++ inventory/kits/views.py
@@ -1,35 +1,151 @@
-# import json
-# import csv
-# from django.contrib.auth.decorators import login_required
-# from django.db import DatabaseError, transaction
-# from django.forms.models import model_to_dict
-# from django.http import HttpResponse
-# from django.shortcuts import render
-# from rest_framework import status
-# from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework.response import Response
-#
-# from .models import *
-# from .models.Item.ItemModels import Item
-# from .serializers import *
-# from .views_utils import manage_items_change
-#
-#
-# Create your views here.
-# @login_required(login_url="/r'^login/$'")
-# def inventory_index(request):
-#     return render(request, "inventory_index.html")
-#
-#
-# @api_view(["GET"])
-# @permission_classes([IsAuthenticated])
-# def api_items(request):
-#     try:
-#         items_data = ItemSerializer(Item.objects.all(), many=True).data
-#         return Response(items_data, status=status.HTTP_200_OK)
-#     except Exception as e:
-#         return Response(
-#             {"message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
-#
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+
+from .serializers import *
+from ..items.models import *
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def api_kits(request):
+    try:
+        kits = Kit.objects.all()
+        serializer = KitSerializer(kits, many=True)
+        blueprints = Blueprint.objects.values_list('name', flat=True).distinct()
+
+        return Response({
+            'kits': serializer.data,
+            'blueprints': list(blueprints)
+        })
+    except Exception as e:
+        return Response(e.args, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def add_kit(request):
+    try:
+        blueprint_id = request.data.get("blueprint")
+        name = request.data.get("name")
+
+        if not blueprint_id or not name:
+            return Response({"error": "Required parameters are missing!"}, status=status.HTTP_400_BAD_REQUEST)
+
+        blueprint = Blueprint.objects.get(id=blueprint_id)
+
+        Kit.objects.create(
+            name=name,
+            blueprint=blueprint,
+            status="READY",
+            content=blueprint.complete_content,
+        )
+
+        return Response({"message": "Kit added successfully!"},
+                        status=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def retire_kit(request):
+    try:
+        kit_id = request.GET.get("kit_id")
+
+        if not kit_id:
+            return Response({"error": "Required parameters are missing!"}, status=status.HTTP_400_BAD_REQUEST)
+
+        kit = Kit.objects.get(id=kit_id)
+        content = kit.content
+
+        # Check that all items can be deposited
+        for item in content:
+            item_id = item.get("id")
+            quantity = item.get("quantity")
+            if not item_id or quantity is None:
+                raise Exception("Content JSON is missing required data.")
+
+            item_expiry = ItemExpiry.objects.get(id=item_id)
+
+            if item_expiry.quantity is None or quantity < 0 or (item_expiry.quantity + quantity) < 0:
+                raise Exception(f"Cannot deposit {quantity} for item with ID {item_id}.")
+
+        # Deposit all items
+        for item in content:
+            item_id = item.get("id")
+            quantity = item.get("quantity")
+
+            item_expiry = ItemExpiry.objects.get(id=item_id)
+            item_expiry.deposit(quantity)
+
+        kit.status = "RETIRED"
+        kit.save()
+
+        return Response({"message": "Kit retired and contents deposited successfully!"}, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def add_blueprint(request):
+    try:
+        name = request.data.get("name")
+        content = request.data.get("content")
+
+        if not name or not content:
+            return Response({"error": "Required parameters are missing!"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if name already exists
+        if Blueprint.objects.filter(name=name).exists():
+            return Response({"error": "Blueprint with this name already exists!"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if all items in content exist in ItemExpiry
+        for item in content:
+            item_id = item.get("id")
+            if not item_id:
+                raise Exception("Content JSON is missing required data for an item.")
+
+            if not ItemExpiry.objects.filter(id=item_id).exists():
+                return Response({"error": f"Item with ID {item_id} does not exist in ItemExpiry."},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        Blueprint.objects.create(
+            name=name,
+            content=content,
+        )
+
+        return Response({"message": "Blueprint added successfully!"},
+                        status=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def kit_history(request):
+    return Response({"message": "Hello, world!"})
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def submit_kit_order(request):
+    return Response({"message": "Hello, world!"})
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def return_kit_order(request):
+    return Response({"message": "Hello, world!"})
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def revert_kit_order(request):
+    return Response({"message": "Hello, world!"})
