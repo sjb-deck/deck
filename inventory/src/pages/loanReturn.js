b3ed41f6f9583fcc041ec48d7f1f8fedbcab9101
jonasgwt <jongoh2000@gmail.com>
2023-11-06 18:44:18 +0800

    Change img to svg and add empty loans visual

--- inventory/src/pages/loanReturn.js
+++ inventory/src/pages/loanReturn.js
@@ -32,11 +32,45 @@ export const LoanReturn = () => {
     return (
       <Theme>
         <NavBar user={user} />
-        <Stack justifyContent='center' alignItems='center'>
-          <div className='nav-margin-compensate'>
-            <Typography variant='h1'>No active loans</Typography>
+        <Stack justifyContent='center' alignItems='center' minHeight={'100vh'}>
+          <div
+            className='nav-margin-compensate'
+            style={{
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              width: '75%',
+            }}
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              className='bi bi-cart-dash-fill'
+              viewBox='0 0 16 16'
+              style={{ width: '15vw', height: '15vw', marginBottom: '5vw' }}
+            >
+              <path d='M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM6.5 7h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1 0-1z' />
+            </svg>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                marginBottom: '5vw',
+              }}
+            >
+              <Typography variant='h3' gutterBottom>
+                No active loans
+              </Typography>
+              <Typography variant='body'>
+                Make a loan order and you will see it here!
+              </Typography>
+            </div>
           </div>
         </Stack>
+        <Footer />
       </Theme>
     );
   }
