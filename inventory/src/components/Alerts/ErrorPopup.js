7687bf380d0a9828794e4bc1a0a4e9ddf9b18b82
jonasgwt <jongoh2000@gmail.com>
2023-11-18 02:36:37 +0800

    Add max height to error display

--- inventory/src/components/Alerts/ErrorPopup.js
+++ inventory/src/components/Alerts/ErrorPopup.js
@@ -1,26 +1,35 @@
-import { Alert, AlertTitle, Backdrop, Typography } from '@mui/material';
+import { Alert, AlertTitle, Backdrop, Box, Typography } from '@mui/material';
 import React from 'react';
 
 export const ErrorPopup = ({ open, message, additionalInfo }) => {
+  const handleClose = () => {
+    window.location.reload();
+  };
+
   return (
     <Backdrop
       sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer * 2 }}
       open={open}
+      onClick={handleClose}
     >
       <Alert variant='filled' severity='error' sx={{ maxWidth: '90%' }}>
         <AlertTitle>Whoops! Something went wrong</AlertTitle>
         <Typography>We are unable to process this request!</Typography>
 
         <Typography marginTop={3}>Error Message:</Typography>
-        <Typography variant='overline' gutterBottom>
-          {message}
-        </Typography>
+        <Box maxHeight={'25vh'} overflow={'scroll'}>
+          <Typography variant='overline' gutterBottom>
+            {message}
+          </Typography>
+        </Box>
         {additionalInfo && (
           <>
             <Typography marginTop={3}>Backend Response:</Typography>
-            <Typography variant='overline' gutterBottom>
-              {additionalInfo}
-            </Typography>
+            <Box maxHeight={'25vh'} overflow={'scroll'}>
+              <Typography variant='overline' gutterBottom>
+                {additionalInfo}
+              </Typography>
+            </Box>
           </>
         )}
 
