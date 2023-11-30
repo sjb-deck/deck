c2f076b04cb02bd8ebc707aad12d4d6edb210235
jonasgwt <jongoh2000@gmail.com>
2023-11-30 12:59:26 +0800

    Switch back to informative error popup for csv upload

--- inventory/src/hooks/mutations/useImportItems.js
+++ inventory/src/hooks/mutations/useImportItems.js
@@ -1,17 +1,23 @@
 import { useMutation } from '@tanstack/react-query';
+import { useContext } from 'react';
 
 import { Api } from '../../globals';
+import { AlertContext } from '../../providers';
 import { getRequest } from '../../utils/getRequest';
 
 export const useImportItems = (options) => {
   const key = 'importItems';
   const url = Api[key];
   const request = getRequest();
+  const { setAlert } = useContext(AlertContext);
   const defaultOptions = {
     onError: (error) => {
-      alert(
-        'There seems to be an error in processing your CSV. Please check the format and try again.',
-      );
+      setAlert({
+        severity: 'error',
+        message: error.message,
+        autoHide: false,
+        additionalInfo: error.response?.data?.message,
+      });
     },
   };
 
