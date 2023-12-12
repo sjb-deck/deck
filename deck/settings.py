07a15508dfc1ed9abd25810824aa5524efb630eb
cheehengk <cheehengk@yahoo.com.sg>
2023-12-11 22:23:40 -0500

    Fix deck settings

--- deck/settings.py
+++ deck/settings.py
@@ -92,24 +92,16 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-if sys.argv[1] == "test":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": ":memory:",
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "NAME": "deck_staging",
-            "HOST": config("DB_HOST"),
-            "PORT": "3306",
-            "USER": config("DB_USER"),
-            "PASSWORD": config("DB_PW"),
-        },
-    }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "deck_staging",
+        "HOST": config("DB_HOST"),
+        "PORT": "3306",
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PW"),
+    },
+}
 
 # uncomment this to use local db
 # DATABASES = {
