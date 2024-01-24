056f8f153c44eef29f586aae150bcc1ba64e05c1
niclee1803 <136572531+niclee1803@users.noreply.github.com>
2024-01-24 21:39:46 +0800

    Floating cart button update (#190)
    added a shadow to give button depth

--- inventory/src/components/Cart/FloatingCartButton/FloatingCart.js
+++ inventory/src/components/Cart/FloatingCartButton/FloatingCart.js
@@ -6,7 +6,7 @@
  */
 
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Badge, Button } from '@mui/material';
+import { Badge, Fab } from '@mui/material';
 import React, { useContext } from 'react';
 
 import { URL_INV_CART } from '../../../globals';
@@ -16,7 +16,7 @@ export const FloatingCart = () => {
   const { cartItems } = useContext(CartContext);
 
   return (
-    <Button
+    <Fab
       onClick={() => (window.location.href = URL_INV_CART)}
       variant='contained'
       style={{
@@ -28,15 +28,20 @@ export const FloatingCart = () => {
         height: '60px',
         width: '60px',
         zIndex: 1000,
+        background:
+          'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,232,255,1) 0%, rgba(72,116,197,1) 100%, rgba(30,155,173,1) 100%, rgba(93,97,209,1) 100%, rgba(2,3,6,1) 100%)',
+        color: 'black',
+        boxShadow: '0px 0px 10px rgba(255, 183, 0, 0.8)',
       }}
     >
       <Badge
         color='secondary'
         badgeContent={cartItems.length == 0 ? 0 : cartItems.length}
         showZero
+        invisible={cartItems.length === 0}
       >
         <ShoppingCartIcon style={{ fontSize: 40 }} />
       </Badge>
-    </Button>
+    </Fab>
   );
 };
