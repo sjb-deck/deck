d20a916c8aa7c58acb71ee37bc03bad9576511bb
Tong Zheng Hong <zhtong315@gmail.com>
2024-02-05 19:16:35 +0800

    Update style of KitInfoSection

--- inventory/src/components/KitLoanReturn/KitInfoSection.js
+++ inventory/src/components/KitLoanReturn/KitInfoSection.js
@@ -12,12 +12,12 @@ export const KitInfoSection = ({ kitData }) => {
     <Stack
       direction={'column'}
       alignItems={'center'}
-      sx={{ padding: isMobile ? 2 : 8 }}
+      sx={{ p: isMobile ? 2 : 8, pt: 10 }}
     >
       <Avatar {...stringAvatar(kitData?.name)} />
-      <Typography variant='h3'>{kitData?.name}</Typography>
-      <Typography variant='h6'>{kitData?.blueprint_name}</Typography>
-      <Typography variant='overline'>
+      <Typography variant='h3' align="center" noWrap>{kitData?.name}</Typography>
+      <Typography variant='h6' align="center" noWrap>{kitData?.blueprint_name}</Typography>
+      <Typography variant='overline' align="center" noWrap>
         {kitData?.status}, {kitData?.complete}
       </Typography>
     </Stack>
