cacc452e8731d4fbc3324bcfa16216a9544dd0d3
jonasgwt <jongoh2000@gmail.com>
2024-03-24 19:20:54 +0800

    Update tests

--- inventory/src/__tests__/components/KitInfo/ViewExistingBlueprintTable.test.js
+++ inventory/src/__tests__/components/KitInfo/ViewExistingBlueprintTable.test.js
@@ -18,44 +18,33 @@ describe('<ViewExistingBlueprintTable />', () => {
   );
   it('renders blueprint list correctly', () => {
     render(<ViewExistingBlueprintTable items={exampleBlueprint.blueprints} />);
-    // check if header is rendered
-    expect(screen.getByText('Blueprints')).toBeInTheDocument();
     // check if blueprints are rendered
-    const itemNames = screen.queryAllByTestId('item_name');
-    expect(itemNames.length > 0).toBe(true);
-    expect(itemNames.some((ele) => ele.textContent == 'Blueprint Test1')).toBe(
-      true,
-    );
-    expect(itemNames.some((ele) => ele.textContent == 'Blueprint Test2')).toBe(
-      true,
-    );
-    expect(itemNames.some((ele) => ele.textContent == 'Arts')).toBe(true);
-  });
-
-  it('renders blueprint list correctly when list is empty', () => {
-    render(<ViewExistingBlueprintTable items={[]} />);
-    // check that headers is rendered
-    expect(screen.queryByText('Blueprints')).toBeInTheDocument();
+    for (const blueprint of exampleBlueprint.blueprints) {
+      expect(screen.getByText(blueprint.name)).toBeInTheDocument();
+    }
   });
 
   it('renders blueprint content correctly upon clicking', async () => {
     render(<ViewExistingBlueprintTable items={exampleBlueprint.blueprints} />);
-    const blueprints = screen.queryAllByTestId('item_accordion');
-    // check that none of the blueprint content is rendered
-    expect(
-      blueprints.every((ele) => ele.getAttribute('aria-expanded') == 'false'),
-    ).toBe(true);
-    // check that blueprint content is rendered upon clicking
-    await userEvent.click(blueprints[0]);
-    expect(
-      blueprints.some((ele) => ele.getAttribute('aria-expanded') == 'true'),
-    ).toBe(true);
-    expect(screen.getByText('Bandage Test')).toBeVisible();
-    expect(screen.getByText('Tape Test')).toBeVisible();
-    expect(screen.getByText('Test Fail Paper')).not.toBeVisible();
-    expect(screen.getByText('Test Fail Scissors')).not.toBeVisible();
-    const contentQty = screen.queryAllByTestId('blueprint_item_qty');
-    expect(contentQty.some((ele) => ele.textContent == 'test5')).toBe(true);
-    expect(contentQty.some((ele) => ele.textContent == 'test2')).toBe(true);
+    // check that blueprints buttons are rendered
+    for (const blueprint of exampleBlueprint.blueprints) {
+      expect(
+        screen.getByRole('button', {
+          name: `${blueprint.id} ${blueprint.name}`,
+        }),
+      ).toBeInTheDocument();
+    }
+
+    // click on the first blueprint
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: `${exampleBlueprint.blueprints[0].id} ${exampleBlueprint.blueprints[0].name}`,
+      }),
+    );
+
+    // check that blueprint content is rendered
+    for (const item of exampleBlueprint.blueprints[0].complete_content) {
+      expect(screen.getByText(item.name)).toBeInTheDocument();
+    }
   });
 });
