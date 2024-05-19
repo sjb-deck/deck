8a5fb16c76abd06a1d4ddc0e5edbdfbf98ba49f8
jonasgwt <jongoh2000@gmail.com>
2024-05-19 23:04:33 +0800

    Redesign inventory index

--- inventory/src/pages/inventoryIndex.js
+++ inventory/src/pages/inventoryIndex.js
@@ -1,18 +1,22 @@
 import CategoryIcon from '@mui/icons-material/Category';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import HomeIcon from '@mui/icons-material/Home';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import LogoutIcon from '@mui/icons-material/Logout';
 import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { LoadingButton } from '@mui/lab';
 import {
+  Avatar,
+  Badge,
   Box,
-  Button,
-  Fab,
   Stack,
   ThemeProvider,
+  Tooltip,
+  Typography,
   createTheme,
+  useMediaQuery,
 } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   URL_INV_ITEMS,
@@ -20,6 +24,8 @@ import {
   URL_INV_VIEW_KITS,
   URL_INV_VIEW_ORDERS_LOANS,
 } from '../globals';
+import { useUser } from '../hooks/queries';
+import { stringAvatar } from '../utils';
 
 export const InventoryIndex = () => {
   const lightTheme = createTheme({
@@ -33,23 +39,10 @@ export const InventoryIndex = () => {
       },
     },
   });
+  const { data: user } = useUser();
+  const isMobile = useMediaQuery('(max-width:600px)');
   return (
     <ThemeProvider theme={lightTheme}>
-      <Fab
-        variant='extended'
-        sx={{ position: 'absolute', top: 15, left: 15 }}
-        href='/'
-      >
-        <HomeIcon sx={{ mr: 2 }} />
-        Apps
-      </Fab>
-      <Fab
-        variant='extended'
-        sx={{ position: 'absolute', top: 15, right: 15 }}
-        href={URL_INV_VIEW_ORDERS_LOANS}
-      >
-        <DashboardIcon />
-      </Fab>
       <Box
         sx={{
           width: '100vw',
@@ -64,6 +57,8 @@ export const InventoryIndex = () => {
           alignItems: 'center',
         }}
       >
+        <InventoryIndexUserCard user={user} isMobile={isMobile} />
+
         <img
           src='/static/inventory/img/logo.png'
           alt='logo'
@@ -83,47 +78,184 @@ export const InventoryIndex = () => {
         >
           Welcome to IMS
         </h1>
-        <Stack spacing={2} sx={{ pt: 2 }}>
-          <Button
-            variant='outlined'
-            startIcon={<CategoryIcon />}
-            href={URL_INV_ITEMS}
-            size='large'
-            sx={{ fontSize: '1rem', width: '200px' }}
-          >
-            <Box ml={2}>Items</Box>
-          </Button>
-          <Button
-            variant='outlined'
-            size='large'
-            startIcon={<MedicalServicesIcon />}
-            href={URL_INV_VIEW_KITS}
-            sx={{ fontSize: '1rem', width: '200px' }}
-          >
-            <Box ml={2}>Kits&nbsp;</Box>
-          </Button>
-          <Button
-            variant='outlined'
-            size='large'
-            startIcon={<KeyboardReturnIcon />}
-            href={URL_INV_LOAN_RETURN}
-            sx={{ fontSize: '1rem', width: '200px' }}
-          >
-            <Box ml={2}>Loans</Box>
-          </Button>
-        </Stack>
+        <InventoryIndexOptions />
       </Box>
-      <Fab
-        color='primary'
-        aria-label='alerts'
+    </ThemeProvider>
+  );
+};
+
+export const InventoryIndexOptions = () => {
+  const [loadingItems, setLoadingItems] = useState(false);
+  const [loadingKits, setLoadingKits] = useState(false);
+  const [loadingLoans, setLoadingLoans] = useState(false);
+  const [loadingDashboard, setLoadingDashboard] = useState(false);
+  const [loadingNotifications, setLoadingNotifications] = useState(false);
+  return (
+    <Stack spacing={2} sx={{ pt: 2 }}>
+      <LoadingButton
+        variant='outlined'
+        startIcon={<CategoryIcon />}
+        onClick={() => {
+          setLoadingItems(true);
+          window.location.href = URL_INV_ITEMS;
+        }}
+        size='large'
+        loading={loadingItems}
+        loadingPosition='end'
+        sx={{ fontSize: '1rem', width: '200px' }}
+      >
+        <Box ml={2}>Items</Box>
+      </LoadingButton>
+      <LoadingButton
+        variant='outlined'
+        startIcon={<MedicalServicesIcon />}
+        onClick={() => {
+          setLoadingKits(true);
+          window.location.href = URL_INV_VIEW_KITS;
+        }}
+        size='large'
+        loading={loadingKits}
+        loadingPosition='end'
+        sx={{ fontSize: '1rem', width: '200px' }}
+      >
+        <Box ml={2}>Kits&nbsp;</Box>
+      </LoadingButton>
+      <LoadingButton
+        variant='outlined'
+        startIcon={<KeyboardReturnIcon />}
+        onClick={() => {
+          setLoadingLoans(true);
+          window.location.href = URL_INV_LOAN_RETURN;
+        }}
+        size='large'
+        loading={loadingLoans}
+        loadingPosition='end'
+        sx={{ fontSize: '1rem', width: '200px' }}
+      >
+        <Box ml={2}>Loans</Box>
+      </LoadingButton>
+      <Stack direction='row' spacing={1} sx={{ justifyContent: 'center' }}>
+        <LoadingButton
+          variant='contained'
+          size='large'
+          onClick={() => {
+            setLoadingDashboard(true);
+            window.location.href = URL_INV_VIEW_ORDERS_LOANS;
+          }}
+          loading={loadingDashboard}
+          loadingPosition='end'
+          sx={{ width: '50%' }}
+        >
+          <DashboardIcon />
+        </LoadingButton>
+        <LoadingButton
+          variant='contained'
+          size='large'
+          aria-label='alerts'
+          onClick={() => {
+            setLoadingNotifications(true);
+            // Add the action for the notifications button here
+          }}
+          loading={loadingNotifications}
+          loadingPosition='end'
+          sx={{ width: '50%' }}
+        >
+          <Badge badgeContent={1} color='error'>
+            <NotificationsIcon />
+          </Badge>
+        </LoadingButton>
+      </Stack>
+    </Stack>
+  );
+};
+
+export const InventoryIndexUserCard = ({ user, isMobile }) => {
+  const mobileStyle = {
+    position: 'absolute',
+    top: 15,
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    paddingTop: '10px',
+    paddingBottom: '10px',
+    boxShadow: '0 6px 12px rgb(0 0 0 / 23%), 0 10px 40px rgb(0 0 0 / 19%)',
+    borderRadius: '15px',
+    minWidth: '200px',
+  };
+  const desktopStyle = {
+    position: 'absolute',
+    top: 15,
+    right: 15,
+    boxShadow:
+      '0 6px 12px rgb(0 0 0 / 23%), 0 10px 40px rgb(255 255 255 / 19%)',
+    borderRadius: '15px',
+    minWidth: '300px',
+    backgroundColor: 'rgb(255 255 255 / 85%)',
+    padding: '15px 20px',
+  };
+  return (
+    <Box
+      sx={isMobile ? mobileStyle : desktopStyle}
+      elevation={24}
+      square='false'
+    >
+      <Stack
+        direction='row'
+        spacing={2}
         sx={{
-          position: 'absolute',
-          bottom: 15,
-          right: 15,
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
         }}
       >
-        <NotificationsIcon />
-      </Fab>
-    </ThemeProvider>
+        <Tooltip title='Manage Account Details'>
+          <Avatar
+            alt='User'
+            src='/static/img/blank_profile.webp'
+            {...stringAvatar(user?.username || 'User')}
+            sx={{
+              width: 80,
+              height: 80,
+              '&:hover': {
+                filter: 'brightness(0.8)',
+                cursor: 'pointer',
+              },
+            }}
+            onClick={() => {
+              window.location.href = '/accounts/edit';
+            }}
+          />
+        </Tooltip>
+        <Stack spacing={0}>
+          <Box
+            sx={{
+              fontSize: '1rem',
+              color: 'black',
+            }}
+          >
+            {user ? user?.username : 'Loading...'}
+          </Box>
+          <Typography
+            variant='caption'
+            sx={{
+              color: 'black',
+              p: 0,
+            }}
+          >
+            {user ? user?.extras?.role : 'Role'}
+          </Typography>
+          <LoadingButton
+            variant='outlined'
+            size='small'
+            onClick={() => {
+              window.location.href = '/logout';
+            }}
+            sx={{ fontSize: '0.75rem', marginTop: '5px' }}
+            color='error'
+            startIcon={<LogoutIcon />}
+          >
+            Logout
+          </LoadingButton>
+        </Stack>
+      </Stack>
+    </Box>
   );
 };
