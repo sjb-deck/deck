86675686a427d2be9ae1907932fcfb648eb6a2dd
jonasgwt <jongoh2000@gmail.com>
2024-06-22 13:26:48 +0800

    chore: make django admin page accessible

--- backend/deck/urls.py
+++ backend/deck/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = (
-    [path("admin/", admin.site.urls), path("api/v1/", include("deck.app_urls"))]
+    [path("api/admin/", admin.site.urls), path("api/v1/", include("deck.app_urls"))]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
