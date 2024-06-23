698c64c9f9c52477209a3431c000f6bd8e110bde
Jonas Goh <55698556+jonasgwt@users.noreply.github.com>
2024-06-23 23:43:48 +0800

    Refactor (#290)

--- run.sh
+++ run.sh
@@ -21,19 +21,26 @@ handle_error() {
 
 cleanup() {
     print_msg "${RED}Cleaning up...${NC}"
-    if [ "$MODE" == "prod" ]; then
-        docker-compose -f docker-compose-prod.yml down
+    if [ "$MODE" == "prod" ] || [ "$MODE" == "staging" ]; then
+        docker-compose -f docker-compose.yml down
     else
         docker-compose down
     fi
     print_msg "${RED}Containers have been stopped.${NC}"
+
+    # Remove the generated nginx.prod.conf file
+    if [ -f frontend/nginx.prod.conf ]; then
+        rm frontend/nginx.prod.conf
+        print_msg "${GREEN}Removed generated nginx.prod.conf file.${NC}"
+    fi
 }
 
 build_and_push() {
     IMAGE_NAME=$1
     CONTEXT_DIR=$2
+    TAG=$3
 
-    print_msg "${YELLOW}Building and pushing Docker image for ${IMAGE_NAME} from ${CONTEXT_DIR}${NC}"
+    print_msg "${YELLOW}Building and pushing Docker image for ${IMAGE_NAME}:${TAG} from ${CONTEXT_DIR}${NC}"
 
     docker buildx inspect mybuilder > /dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -44,21 +51,42 @@ build_and_push() {
     print_msg "${GREEN}Ensuring the Docker buildx builder instance is started...${NC}"
     docker buildx inspect mybuilder --bootstrap || handle_error "Failed to start Docker buildx builder instance"
 
-    print_msg "${GREEN}Building and pushing the image for ${IMAGE_NAME}...${NC}"
+    print_msg "${GREEN}Building and pushing the image for ${IMAGE_NAME}:${TAG}...${NC}"
     docker buildx build --platform linux/amd64,linux/arm64 \
-        -t sjbdeck/${IMAGE_NAME}:latest \
+        -t sjbdeck/${IMAGE_NAME}:${TAG} \
         --push \
-        ${CONTEXT_DIR} || handle_error "Failed to build and push ${IMAGE_NAME}"
+        ${CONTEXT_DIR} || handle_error "Failed to build and push ${IMAGE_NAME}:${TAG}"
 
-    print_msg "${GREEN}Successfully built and pushed ${IMAGE_NAME}${NC}"
+    print_msg "${GREEN}Successfully built and pushed ${IMAGE_NAME}:${TAG}${NC}"
 
     print_msg "${GREEN}Removing the Docker buildx builder instance...${NC}"
     docker buildx rm mybuilder || handle_error "Failed to remove Docker buildx builder instance"
 }
 
+configure_nginx() {
+    DOMAIN=$1
+
+    print_msg "${GREEN}Configuring Nginx for domain: ${DOMAIN}${NC}"
+    sed -e "s/\${DOMAIN_NAME}/${DOMAIN}/g" frontend/nginx.prod.template.conf > frontend/nginx.prod.conf || handle_error "Failed to configure Nginx"
+}
+
 if [ "$1" == "--prod" ]; then
     MODE="prod"
+    TAG="latest"
+    DOMAIN="deck.nhhs-sjb.org"
     print_msg "${YELLOW}Entering production mode...${NC}"
+elif [ "$1" == "--staging" ]; then
+    MODE="staging"
+    TAG="staging"
+    DOMAIN="deck-stg.nhhs-sjb.org"
+    print_msg "${YELLOW}Entering staging mode...${NC}"
+else
+    MODE="dev"
+    print_msg "${YELLOW}Entering development mode...${NC}"
+fi
+
+if [ "$MODE" == "prod" ] || [ "$MODE" == "staging" ]; then
+    configure_nginx $DOMAIN
 
     print_msg "${GREEN}Changing to frontend directory...${NC}"
     cd frontend || handle_error "Failed to change directory to frontend"
@@ -72,17 +100,12 @@ if [ "$1" == "--prod" ]; then
     print_msg "${GREEN}Changing back to the root directory...${NC}"
     cd .. || handle_error "Failed to change back to the root directory"
 
-    build_and_push "deck-frontend" "./frontend"
+    build_and_push "deck-frontend" "./frontend" "$TAG"
+    build_and_push "deck-backend" "./backend" "$TAG"
 
-    build_and_push "deck-backend" "./backend"
-
-    print_msg "${GREEN}Production images have been built and pushed successfully.${NC}"
+    print_msg "${GREEN}${MODE} images have been built and pushed successfully.${NC}"
 else
-    MODE="dev"
-    print_msg "${YELLOW}Entering development mode...${NC}"
-
     print_msg "${GREEN}Bringing up the development environment with Docker Compose...${NC}"
     docker-compose up --build || handle_error "Failed to bring up the development environment with Docker Compose"
-
-    print_msg "${GREEN}Development environment is up and running${NC}"
+    print_msg "${GREEN}Development environment is up and running${NC}" # TODO
 fi
