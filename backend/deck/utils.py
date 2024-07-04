c3574b7f4888d9f5efcf05d31c2da59206a35a32
jonasgwt <jongoh2000@gmail.com>
2024-07-04 23:50:32 +0800

    Fix S3 integration for prod

--- backend/deck/utils.py
+++ backend/deck/utils.py
@@ -21,3 +21,20 @@ def delete_file(file_path):
         print("Credentials not available", flush=True)
     except ClientError as e:
         print(e, flush=True)
+
+
+def get_s3_client():
+    if settings.ENV == "prod":
+        # In production, use IAM roles
+        return boto3.client(
+            "s3",
+            region_name=settings.AWS_S3_REGION_NAME,
+        )
+    else:
+        # In staging, use access keys
+        return boto3.client(
+            "s3",
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            region_name=settings.AWS_S3_REGION_NAME,
+        )
