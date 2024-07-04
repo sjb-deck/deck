919f7cc9ce9a9d0fdf1ccd9175a4464e38950f3e
jonasgwt <jongoh2000@gmail.com>
2024-07-04 22:00:43 +0800

    Implement delete image helper method

--- backend/deck/utils.py
+++ backend/deck/utils.py
@@ -1,7 +1,18 @@
-import requests
-from requests.auth import HTTPBasicAuth
-from decouple import config
+import boto3
+from django.conf import settings
+from botocore.exceptions import NoCredentialsError, ClientError
 
 
 def delete_file(file_path):
-    pass
+    try:
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            region_name=settings.AWS_S3_REGION_NAME,
+        )
+        s3_client.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=file_path)
+    except NoCredentialsError:
+        print("Credentials not available", flush=True)
+    except ClientError as e:
+        print(e, flush=True)
