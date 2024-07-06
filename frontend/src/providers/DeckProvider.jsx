8d29ae198112108fcbe494178d23548b24314910
jonasgwt <jongoh2000@gmail.com>
2024-07-06 23:47:43 +0800

    Update createRefresh with payload shape and interval time

--- frontend/src/providers/DeckProvider.jsx
+++ frontend/src/providers/DeckProvider.jsx
@@ -16,16 +16,15 @@ import { KitCartProvider } from './KitCartProvider';
 const queryClient = new QueryClient();
 
 const refresh = createRefresh({
-  interval: 10,
+  interval: 5,
   refreshApiCallback: async (param) => {
     try {
-      const response = await axios.post(Api.refresh, param, {
+      const response = await axios.post(Api.refresh, {refresh: param.refreshToken}, {
         headers: { Authorization: `Bearer ${param.authToken}` },
       });
-      console.log('refreshing');
       return {
         isSuccess: true,
-        newAuthToken: response.data.token,
+        newAuthToken: response.data.access,
         newAuthTokenExpireIn: 10,
         newRefreshTokenExpiresIn: 60,
       };
