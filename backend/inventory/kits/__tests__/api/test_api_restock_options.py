ea21fdf9669218a251451af0ca5e22d7337ef660
jonasgwt <jongoh2000@gmail.com>
2024-08-03 23:17:24 +0800

    Fix BE tests

--- backend/inventory/kits/__tests__/api/test_api_restock_options.py
+++ backend/inventory/kits/__tests__/api/test_api_restock_options.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from accounts.models import User, UserExtras
 from inventory.items.models import Item, ItemExpiry
 from inventory.kits.models import Blueprint, Kit, History
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class TestApiRestockOptionsViews(TestCase):
@@ -12,7 +13,8 @@ class TestApiRestockOptionsViews(TestCase):
         self.user = User.objects.create_user(
             username="testuser", password="testpass", email="testuser@example.com"
         )
-        self.client.login(username="testuser", password="testpass")
+        self.token = str(RefreshToken.for_user(self.user).access_token)
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.token)
 
         self.create_items()
         self.compressed_blueprint_content = [
