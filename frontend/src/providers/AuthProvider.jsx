8f1e6d806388cde376ff48371872ba545df2cdb0
WeeMingQing <e0725595@u.nus.edu>
2024-08-10 10:55:21 +0800

    Fix error

--- frontend/src/providers/AuthProvider.jsx
+++ frontend/src/providers/AuthProvider.jsx
@@ -1,61 +1,58 @@
-import { createContext, useEffect, useContext } from 'react';
 import { useMutation } from '@tanstack/react-query';
-import { AlertContext } from './AlertProvider';
 import axios from 'axios';
+import { createContext, useEffect, useContext } from 'react';
+
 import { Api } from '../globals/api';
 import { staySignedIn, signOut, getRefreshToken } from '../hooks/auth/authHook';
+
+import { AlertContext } from './AlertProvider';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const { setAlert } = useContext(AlertContext);
-    const refreshAuthToken = (options) => {
-        const defaultOptions = {
-            refetchOnWindowFocus: false,
-            onError: (error) => {
-                signOut();
-                if (error.response?.status === 401) return;
-                console.error(error);
-                setAlert({
-                    severity: 'error',
-                    message: error.message,
-                    autoHide: false,
-                    additionalInfo: error.response?.data?.message,
-                });
-            },
-            onSuccess: (data) => {
-                staySignedIn(data.access, data.refresh);
-            },
-        };
-        return useMutation({
-            mutationFn: async (data) => {
-              const response = await axios.post(Api['refresh'], data);
-              return response.data;
-            },
-            ...defaultOptions,
-            ...options,
+  const { setAlert } = useContext(AlertContext);
+  const useRefreshAuthToken = (options) => {
+    const defaultOptions = {
+      refetchOnWindowFocus: false,
+      onError: (error) => {
+        signOut();
+        if (error.response?.status === 401) return;
+        console.error(error);
+        setAlert({
+          severity: 'error',
+          message: error.message,
+          autoHide: false,
+          additionalInfo: error.response?.data?.message,
         });
-    }
+      },
+      onSuccess: (data) => {
+        staySignedIn(data.access, data.refresh);
+      },
+    };
+    return useMutation({
+      mutationFn: async (data) => {
+        const response = await axios.post(Api['refresh'], data);
+        return response.data;
+      },
+      ...defaultOptions,
+      ...options,
+    });
+  };
 
-    const {mutate} = refreshAuthToken();
+  const { mutate } = useRefreshAuthToken();
 
-    useEffect(() => {
-        const useCheckTokenExpiration = () => {
-            if (getRefreshToken()) {
-                mutate({refresh: getRefreshToken()});
-            } else {
-                signOut();
-            }
-        }
-        useCheckTokenExpiration();
-        const interval = setInterval(useCheckTokenExpiration, 1100000);
-        return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    const checkTokenExpiration = () => {
+      if (getRefreshToken()) {
+        mutate({ refresh: getRefreshToken() });
+      } else {
+        signOut();
+      }
+    };
+    checkTokenExpiration();
+    const interval = setInterval(checkTokenExpiration, 1100000);
+    return () => clearInterval(interval);
+  }, [mutate]);
 
-    return (
-        <AuthContext.Provider>
-            {children}
-        </AuthContext.Provider>
-    )
-
-}
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
